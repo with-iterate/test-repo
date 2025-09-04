aefa802ccbc3c2552752a6b5453ccbbea681c3cf
with-iterate <withiterate@icloud.com>
2025-09-04 10:13:23 -0700

    Remove multiply function from math utility

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,6 +4,3 @@ export function add(a: number, b: number): number {
 export function divide(a: number, b: number): number {
     return a / b;
 }
-export function multiply(a: number, b: number): number {
-    return a * b;
-}
